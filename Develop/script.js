bd27860204fd1bf2d7fcc184928fc7d2c37df458
Maria Cardona <mechas8703@hotmail.com>
2021-11-09 22:03:47 -0500

    confirmation factors

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
+// Var LowerCase ----------------
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Var UpperCase ----------------
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
+//--------------------------------------
+
+//Var Numbers---------------------------
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Var Special--------------------------
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
